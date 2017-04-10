4a31173464b3e66b08da75c67675088742988946
jan-matthis <mail@jan-matthis.de>
2017-04-10 19:15:38 +0200

    remove visdom code

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='lfmods',
-    version='0.0.2.dev0',
+    version='0.0.3.dev0',
     description='Applications of likelihoodfree to different problems',
     url='https://github.com/mackelab/likelihoodfree-applications',
-    install_requires=['allensdk', 'likelihoodfree', 'h5py', 'visdom'],
+    install_requires=['allensdk', 'likelihoodfree', 'h5py'],
     extras_require={
         'dev': ['check-manifest'],
         'test': ['coverage'],
