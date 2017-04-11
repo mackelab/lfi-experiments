393dba62bdb958d44dce6594bb26a3cbbe1db4d4
jan-matthis <mail@jan-matthis.de>
2017-04-12 00:40:41 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lfmods',
-    version='0.0.3.dev0',
+    version='0.0.4.dev0',
     description='Applications of likelihoodfree to different problems',
     url='https://github.com/mackelab/likelihoodfree-applications',
     install_requires=['allensdk', 'likelihoodfree', 'h5py', 'nbparameterise'],
