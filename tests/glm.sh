6b31473286b883c297511d37325b521a8a86f636
pedro goncalves <ppjgoncalves@gmail.com>
2017-04-13 21:07:36 +0200

    added glm option

---
+++ tests/glm.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+COMMON='--seed 1 --samples 2000 --nb --no-jupyter'
+
+cd ..
+python run.py glm testc1 --rep 2 ${COMMON}
+python run.py glm testc1iw --rep 2 --iw-loss ${COMMON}
+python run.py glm testrnnc1 --rnn 10 --rep 2 ${COMMON}
+python run.py glm testrnnc1iw --rnn 10 --rep 2 --iw-loss ${COMMON}
