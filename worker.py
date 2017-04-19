857ed50de2cdc93a34a954f1430609aad4767330
jan-matthis <mail@jan-matthis.de>
2017-04-19 18:28:26 +0200

    cpu and gpu queues

--- worker.py
+++ worker.py
@@ -2,7 +2,9 @@
 """Worker for jobs
 """
 import click
+import pdb
 import os
+import re
 import subprocess
 
 from redis import Redis
@@ -11,15 +13,24 @@ from rq import Worker, Queue, Connection
 def subprocs_exec(args):
     subprocess.call(args, shell=False)
 
-
 @click.command()
 @click.option('--queue', type=str, default='default', show_default=True,
               help='Queue to listen on')
 def run(queue):
-    connection = Redis()
-
     listen = ['all', queue]
 
+    cpu_regex = re.compile(r"cpu(\d)+")
+    gpu_regex = re.compile(r"gpu(\d)+")
+
+    if cpu_regex.search(queue) is not None:
+        listen += ['cpu']
+
+    if gpu_regex.search(queue) is not None:
+        listen += ['gpu']
+        os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(queue[3:])
+
+    connection = Redis()
     with Connection(connection):
         worker = Worker(map(Queue, listen))
         worker.work()
