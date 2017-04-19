87389070fd740af5b0096b4224f4272b2292157b
jan-matthis <mail@jan-matthis.de>
2017-04-19 19:37:39 +0200

    strict listening option

--- worker.py
+++ worker.py
@@ -16,19 +16,24 @@ def subprocs_exec(args):
 @click.command()
 @click.option('--queue', type=str, default='default', show_default=True,
               help='Queue to listen on')
-def run(queue):
-    listen = ['all', queue]
+@click.option('--strict', default=False, is_flag=True, show_default=True,
+              help='If set, will only listen on specified queue.')
+def run(queue, strict):
+    if not strict:
+        listen = ['all', queue]
 
-    cpu_regex = re.compile(r"cpu(\d)+")
-    gpu_regex = re.compile(r"gpu(\d)+")
+        cpu_regex = re.compile(r"cpu(\d)+")
+        gpu_regex = re.compile(r"gpu(\d)+")
 
-    if cpu_regex.search(queue) is not None:
-        listen += ['cpu']
+        if cpu_regex.search(queue) is not None:
+            listen += ['cpu']
 
-    if gpu_regex.search(queue) is not None:
-        listen += ['gpu']
-        os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-        os.environ["CUDA_VISIBLE_DEVICES"] = str(queue[3:])
+        if gpu_regex.search(queue) is not None:
+            listen += ['gpu']
+            os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(queue[3:])
+    else:
+        listen = [queue]
 
     connection = Redis()
     with Connection(connection):
