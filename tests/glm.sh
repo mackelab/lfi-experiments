7e84ae11f70a09f08c565da3c173f1973e6fae12
pedro goncalves <ppjgoncalves@gmail.com>
2017-05-06 20:39:31 +0200

    updated parameters for method

--- tests/glm.sh
+++ tests/glm.sh
@@ -1,9 +1,15 @@
 #/bin/sh
 
-COMMON='--seed 1 --samples 2000 --nb --no-browser'
+COMMON1='--samples 5000 --rep 5 --svi --sim-kwargs len_filter=9,seed_obs=1,seed_input=42 --val 500'
+COMMON2='--samples 5000 --rep 5 --svi --units [50,50] --loss-calib 10,2 --early-stopping --iw-loss --sim-kwargs len_filter=9,seed_obs=1,seed_input=42 --train-kwargs observe="weights",reg_scale=0.1 --val 500'
+
 
 cd ..
-python run.py glm testc1 --rep 2 ${COMMON}
-python run.py glm testc1iw --rep 2 --iw-loss ${COMMON}
-python run.py glm testrnnc1 --rnn 10 --rep 2 ${COMMON}
-python run.py glm testrnnc1iw --rnn 10 --rep 2 --iw-loss ${COMMON}
+
+python run.py glm 10sta_5000_svi_seed1 --seed 1 ${COMMON1}
+python run.py glm 10sta_5000_svi_seed2 --seed 2 ${COMMON1}
+python run.py glm 10sta_5000_svi_seed3 --seed 3 ${COMMON1}
+
+python run.py glm 10sta_5000_iwloss_svi_inc_data_seed1 --seed 1 ${COMMON2}
+python run.py glm 10sta_5000_iwloss_svi_inc_data_seed2 --seed 2 ${COMMON2}
+python run.py glm 10sta_5000_iwloss_svi_inc_data_seed3 --seed 3 ${COMMON2}
