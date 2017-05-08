e9fa7001e721dcc130caea419d74ad11133f9fe6
pedro goncalves <ppjgoncalves@gmail.com>
2017-05-09 01:14:44 +0200

    minor updates

--- tests/sqrt.sh
+++ tests/sqrt.sh
@@ -1,7 +1,8 @@
 #/bin/sh
 
-COMMON='--rep 4 --samples 2000 --seed 1'
+COMMON='--rep 2 --samples 1000 --seed 1'
 
 cd ..
 
-python run.py sqrt testd1iwsvi --iw-loss --svi ${COMMON}
+python run.py sqrt testd1iwsvi --iw-loss --loss-calib 0.3 --svi ${COMMON}
+# python run.py sqrt testd1iwsvi --iw-loss --loss-calib 10 --svi ${COMMON}
