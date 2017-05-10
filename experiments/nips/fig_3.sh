feddd65117ec6f005a0ead3a31c69616fd962431
pedro goncalves <ppjgoncalves@gmail.com>
2017-05-10 17:42:10 +0200

    launch all experiments for fig3

--- experiments/nips/fig_3.sh
+++ experiments/nips/fig_3.sh
@@ -15,7 +15,9 @@ python run.py hh hh_500_iwloss_svi_accudata_cyth_seed2 --seed 2 ${COMMON2}
 python run.py hh hh_500_iwloss_svi_accudata_cyth_seed3 --seed 3 ${COMMON2}
 
 ## genetic algorithm (IBEA)
-
+python run_genetic.py hh hh_500_iwloss_svi_accudata_cyth_seed1
+python run_genetic.py hh hh_500_iwloss_svi_accudata_cyth_seed2
+python run_genetic.py hh hh_500_iwloss_svi_accudata_cyth_seed3
 
 ## real data
 COMMON2='--samples 500 --rep 5 --svi --accumulate-data --iw-loss --sim-kwargs seed_obs=1,seed_input=42,cached_pilot=False,cached_sims=False,obs_sim=False,cython=True --train-kwargs observe="weights",reg_scale=0.1 --val 500'
