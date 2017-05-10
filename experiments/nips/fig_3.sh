a688820e028e5f3d174a7629fd5049d5a4be7593
pedro goncalves <ppjgoncalves@gmail.com>
2017-05-10 15:21:33 +0200

    new prefixes

--- experiments/nips/fig_3.sh
+++ experiments/nips/fig_3.sh
@@ -3,18 +3,22 @@ cd ..
 cd ..
 
 ## HH murray
-COMMON1='--samples 500 --rep 5 --svi --sim-kwargs seed_obs=1,seed_input=42,cached_pilot=False,cached_sims=False --val 500'
-python run.py hh hh_500_svi_seed1 --seed 1 ${COMMON1}
-python run.py hh hh_500_svi_seed2 --seed 2 ${COMMON1}
-python run.py hh hh_500_svi_seed3 --seed 3 ${COMMON1}
+COMMON1='--samples 500 --rep 5 --svi --sim-kwargs seed_obs=1,seed_input=42,cached_pilot=False,cached_sims=False,cython=True --val 500'
+python run.py hh hh_500_svi_cyth_seed1 --seed 1 ${COMMON1}
+python run.py hh hh_500_svi_cyth_seed2 --seed 2 ${COMMON1}
+python run.py hh hh_500_svi_cyth_seed3 --seed 3 ${COMMON1}
 
 ## HH ours
 COMMON2='--samples 500 --rep 5 --svi --accumulate-data --iw-loss --sim-kwargs seed_obs=1,seed_input=42,cached_pilot=False,cached_sims=False,cython=True --train-kwargs observe="weights",reg_scale=0.1 --val 500'
-python run.py hh hh_500_iwloss_svi_inc_data_seed1 --seed 1 ${COMMON2}
-python run.py hh hh_500_iwloss_svi_inc_data_seed2 --seed 2 ${COMMON2}
-python run.py hh hh_500_iwloss_svi_inc_data_seed3 --seed 3 ${COMMON2}
+python run.py hh hh_500_iwloss_svi_accudata_cyth_seed1 --seed 1 ${COMMON2}
+python run.py hh hh_500_iwloss_svi_accudata_cyth_seed2 --seed 2 ${COMMON2}
+python run.py hh hh_500_iwloss_svi_accudata_cyth_seed3 --seed 3 ${COMMON2}
 
 ## genetic algorithm (IBEA)
 
 
 ## real data
+COMMON2='--samples 500 --rep 5 --svi --accumulate-data --iw-loss --sim-kwargs seed_obs=1,seed_input=42,cached_pilot=False,cached_sims=False,obs_sim=False,cython=True --train-kwargs observe="weights",reg_scale=0.1 --val 500'
+python run.py hh hh_500_iwloss_svi_accudata_cyth_realdata_seed1 --seed 1 ${COMMON2}
+python run.py hh hh_500_iwloss_svi_accudata_cyth_realdata_seed2 --seed 2 ${COMMON2}
+python run.py hh hh_500_iwloss_svi_accudata_cyth_realdata_seed3 --seed 3 ${COMMON2}
