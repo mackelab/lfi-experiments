bbed5b4d3bd986f0150b88db793ab26c06343c12
pedro goncalves <ppjgoncalves@gmail.com>
2017-05-10 16:27:39 +0200

    multiple bug fixes

--- run_genetic.py
+++ run_genetic.py
@@ -34,7 +34,7 @@ class hh_evaluator(bpopt.evaluators.Evaluator):
         A = param_list
 
         # simulation
-        sim = HHSimulator(seed=100+seed_lfree,pilot_samples=0.,cached_sims=False, cached_pilot=False)
+        sim = HHSimulator(seed=101,pilot_samples=0.,cached_sims=False, cached_pilot=False)
 
         params = np.array(A)
         hh = sim.bm.HH(-70.,params.reshape(1,-1))
@@ -48,26 +48,27 @@ class hh_evaluator(bpopt.evaluators.Evaluator):
         return np.ndarray.tolist(np.abs(sum_stats - self.target_stats)[0])
 
 def run_deap(I, dt, t, params1, obs_stats, labels_sum_stats,
-             pilot_means, pilot_stds, optim_ibea=1, offspring_size=10, max_ngen=10, dir_ibea='', prefix=''):
+             pilot_means, pilot_stds, optim_ibea=1, offspring_size=10, max_ngen=10, dir_genetic='', prefix=''):
     # choose and run genetic algorithm
     evaluator = hh_evaluator(I, dt, t, obs_stats, labels_sum_stats, pilot_means, pilot_stds, params1)
 
     # choose genetic algorithm
     if optim_ibea==1:
+        algo = 'ibea'
         opt = bpopt.deapext.optimisations.IBEADEAPOptimisation(evaluator,offspring_size=offspring_size)
     else:
+        algo = 'deap'
         opt = bpopt.deapext.optimisations.DEAPOptimisation(evaluator,offspring_size=offspring_size)
 
     final_pop, halloffame, log, hist = opt.run(max_ngen=max_ngen)
 
-    io.save((final_pop,halloffame,log,hist), dir_ibea + str(prefix) + '_ibea_offsp'
-            + str(offspring_size) + '_ngen' + str(max_ngen) + '.pkl')
+    io.save((final_pop,halloffame,log,hist), dir_genetic + str(prefix) + '_' + algo + '.pkl')
 
 @click.command()
 @click.argument('model', type=str)
 @click.argument('prefix', type=str)
 @click.option('--algo', type=str, default='ibea', show_default=True, help="Determines \
-which genetic algorithm is run, so far only ibea is implemented.")
+which genetic algorithm is run, so far only ibea and deap default are implemented.")
 @click.option('--debug', default=True, is_flag=True, show_default=True,
               help='If provided, will enter debugger on error and show more \
 info during runtime.')
@@ -90,19 +91,20 @@ def run(model, prefix, algo, debug, seed):
     np.random.seed(seed)
 
     try:
-        # load data from NSPE model
+        # load data from SNPE model
         dists, infos, losses, nets, posteriors, sims = io.load_prefix(dirs['dir_nets'], prefix)
         sim = io.last(sims)
         obs_stats = sim.obs
         y_obs = sim.obs_trace.reshape(-1,1)
+        labels_sum_stats = sim.labels_sum_stats
         gt = sim.true_params
         t = sim.t
-        I = sim.I_obs.reshape(1,-1)
+        I = sim.I_obs
         dt = sim.dt
 
         # un-logged prior over parameters
-        unlog_prior_min = sim.param_invtransform(l_sims[len(sims)-1][1].prior_min)
-        unlog_prior_max = sim.param_invtransform(l_sims[len(sims)-1][1].prior_max)
+        unlog_prior_min = sim.param_invtransform(sim.prior_min)
+        unlog_prior_max = sim.param_invtransform(sim.prior_max)
 
         n_params = sim.n_params
         labels_params = sim.labels_params
@@ -116,20 +118,20 @@ def run(model, prefix, algo, debug, seed):
 
         if algo == 'ibea':
             print('IBEA for {}/{}'.format(model, prefix))
-
-            sim_step = io.last(infos)['n_samples']
-            num_round = len(infos)
-
             optim_ibea = 1
-            offspring_size = int(sim_step)
-            max_ngen = int(num_round)
-
-            t_sim = time.time()
-            run_deap(I, dt, t, params1, obs_stats, labels_sum_stats,
-                     pilot_means, pilot_stds, optim_ibea, offspring_size, max_ngen, dirs['dir_genetic'], prefix)
-
         else:
-            raise ValueError('{} not implemented'.format(algo))
+            print('Default algorithm of DEAP for {}/{}'.format(model, prefix))
+            optim_ibea = 0
+
+        sim_step = io.last(infos)['n_samples']
+        num_round = len(infos)
+
+        offspring_size = int(sim_step)
+        max_ngen = int(num_round)
+
+        t_sim = time.time()
+        run_deap(I, dt, t, params1, obs_stats, labels_sum_stats,
+                 pilot_means, pilot_stds, optim_ibea, offspring_size, max_ngen, dirs['dir_genetic'], prefix)
     except:
         t, v, tb = sys.exc_info()
         if debug:
