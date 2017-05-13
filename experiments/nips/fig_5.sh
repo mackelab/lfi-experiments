978bb9846b0cdc4284235d24239e11096622139d
jan-matthis <mail@jan-matthis.de>
2017-05-13 20:32:55 +0200

    Update fig_5.sh

--- experiments/nips/fig_5.sh
+++ experiments/nips/fig_5.sh
@@ -1,3 +1,7 @@
 #/bin/sh
 cd ..
 cd ..
+
+python run.py hh shrink_nips_v1_60ms --rep 1 --rnn 25 --samples 1000 --seed 2 --svi --sim-kwargs cython=True,duration=60,seed_input=3,seed_obs=42,step_current=False,cached_pilot=False,cached_sims=False --train-kwargs n_iter=500,n_minibatch=250 --device cuda0 --enqueue gpu
+python run.py hh shrink_nips_v1_120ms --rep 1 --rnn 25 --samples 1000 --seed 2 --svi --sim-kwargs cython=True,duration=120,seed_input=3,seed_obs=42,step_current=False,cached_pilot=False,cached_sims=False --train-kwargs n_iter=500,n_minibatch=250 --device cuda0 --enqueue gpu
+python run.py hh shrink_nips_v1_240ms --rep 1 --rnn 25 --samples 1000 --seed 2 --svi --sim-kwargs cython=True,duration=240,seed_input=3,seed_obs=42,step_current=False,cached_pilot=False,cached_sims=False --train-kwargs n_iter=500,n_minibatch=250 --device cuda0 --enqueue gpu
