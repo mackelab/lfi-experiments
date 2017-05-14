6ae526ab5500d2e8106d492c31f3541714051930
pedro <ppjgoncalves@gmail.com>
2017-05-14 12:46:52 +0200

    update fig3 (jm via pedros ws)

--- experiments/nips/fig_3.sh
+++ experiments/nips/fig_3.sh
@@ -3,19 +3,20 @@ cd ..
 cd ..
 
 ## HH murray
-COMMON1='--samples 500 --rep 5 --svi --sim-kwargs seed_obs=1,seed_input=42,cached_pilot=False,cached_sims=False,cython=True --val 500'
-python run.py hh hh_500_svi_cyth_seed1 --seed 1 ${COMMON1}
-python run.py hh hh_500_svi_cyth_seed2 --seed 2 ${COMMON1}
-python run.py hh hh_500_svi_cyth_seed3 --seed 3 ${COMMON1}
+COMMON1='--samples 1000 --rep 5 --svi --sim-kwargs seed_obs=1,seed_input=42,cached_pilot=False,cached_sims=False,cython=True --val 500 --enqueue all'
+python run.py hh hh_1000_svi_cyth_seed1 --seed 1 ${COMMON1}
+python run.py hh hh_1000_svi_cyth_seed2 --seed 2 ${COMMON1}
+python run.py hh hh_1000_svi_cyth_seed3 --seed 3 ${COMMON1}
 
 ## HH ours and genetic algorithm (IBEA)
-COMMON2='--samples 500 --rep 5 --svi --accumulate-data --iw-loss --genetic --sim-kwargs seed_obs=1,seed_input=42,cached_pilot=False,cached_sims=False,cython=True --train-kwargs observe="weights",reg_scale=0.1 --val 500'
-python run.py hh hh_500_iwloss_svi_accudata_cyth_seed1 --seed 1 ${COMMON2}
-python run.py hh hh_500_iwloss_svi_accudata_cyth_seed2 --seed 2 ${COMMON2}
-python run.py hh hh_500_iwloss_svi_accudata_cyth_seed3 --seed 3 ${COMMON2}
+COMMON2='--samples 1000 --train-kwargs reg_scale=0.1 --prior-alpha 0.1 --rep 5 --svi --iw-loss --genetic --sim-kwargs seed_obs=1,seed_input=42,cached_pilot=False,cached_sims=False,cython=True --enqueue all'
+python run.py hh hh_1000_iwloss_svi_cyth_seed1_v3pa01 --seed 1 ${COMMON2}
+python run.py hh hh_1000_iwloss_svi_cyth_seed2_v3pa01 --seed 2 ${COMMON2}
+python run.py hh hh_1000_iwloss_svi_cyth_seed3_v3pa01 --seed 3 ${COMMON2}
+
 
 ## real data
-COMMON3='--samples 500 --rep 5 --svi --accumulate-data --iw-loss --sim-kwargs seed_obs=1,seed_input=42,cached_pilot=False,cached_sims=False,obs_sim=False,cython=True --train-kwargs observe="weights",reg_scale=0.1 --val 500'
-python run.py hh hh_500_iwloss_svi_accudata_cyth_realdata_seed1 --seed 1 ${COMMON3}
-python run.py hh hh_500_iwloss_svi_accudata_cyth_realdata_seed2 --seed 2 ${COMMON3}
-python run.py hh hh_500_iwloss_svi_accudata_cyth_realdata_seed3 --seed 3 ${COMMON3}
+COMMON3='--samples 1000 --train-kwargs reg_scale=0.1 --prior-alpha 0.1 --rep 5 --svi --iw-loss --sim-kwargs seed_obs=1,seed_input=42,cached_pilot=False,cached_sims=False,obs_sim=False,cython=True --enqueue all'
+python run.py hh hh_1000_iwloss_svi_cyth_realdata_seed1_v3pa01 --seed 1 ${COMMON3}
+python run.py hh hh_1000_iwloss_svi_cyth_realdata_seed2_v3pa01 --seed 2 ${COMMON3}
+python run.py hh hh_1000_iwloss_svi_cyth_realdata_seed3_v3pa01 --seed 3 ${COMMON3}
