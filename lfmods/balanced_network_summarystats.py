ba4b24cf85888bd81540f38df7961c49b3a1757f
janfb <j.f.boelts@gmail.com>
2017-07-20 14:02:41 +0200

    new script for calculating summary stats on saved balanced network simulations.

---
+++ lfmods/balanced_network_summarystats.py
@@ -0,0 +1,67 @@
+from lfmods.balanced_network_utils import *
+
+filename = '1500550717.440481r1t10ree1.p'
+folder = '/Users/Jan/Dropbox/Master/mackelab/code/balanced_clustered_network/data/'
+
+round_dict = load_data(filename, folder)
+
+params = round_dict['params']
+n_rounds = params['n_rounds']
+n_trials = params['n_trials']
+NE = params['NE']
+NI = params['NI']
+
+t = 1.5  # in sec
+delta_t = 1.5  # in sec
+recordings_length = delta_t
+
+# define time windows for spike counts
+# in 50 ms time window
+window_length = 0.05  # in s
+# ignore overlap for now
+n_time_windows = int(recordings_length / window_length)
+
+# also make a matrix that collects all the spike counts, for correlations and ff: rounds x trials x neurons
+spike_count_mat_E = np.zeros((n_rounds, n_trials, NE))
+spike_count_mat_I= np.zeros((n_rounds, n_trials, NI))
+
+spike_count_window_mat_E = np.zeros((n_rounds, n_trials, NE, n_time_windows))
+spike_count_window_mat_I = np.zeros((n_rounds, n_trials, NI, n_time_windows))
+correlations_E = []
+correlations_I = []
+
+for r in range(n_rounds):
+
+    for trial in range(n_trials):
+        trial_dict = round_dict['trial{}'.format(trial)]
+
+        # get spike counts in matrix
+        spike_count_mat_E[r, trial, :] = get_spikecounts_for_time_window(trial_dict['spikes_E'],
+                                                                         t, delta_t) # for E neurons
+        spike_count_mat_I[r, trial, :] = get_spikecounts_for_time_window(trial_dict['spikes_I'],
+                                                                         t, delta_t) # for I neurons
+
+        # get spike counts for sliding time windows
+        spike_count_window_mat_E[r, trial, :, :] = calculate_spike_counts_over_windows(trial_dict['spikes_E'], t,
+                                                                                       delta_t, window_length)
+
+    # calculate correlations
+    correlations_E.append(calculate_correlation_matrix(spike_count_window_mat_E[r, ...]))
+
+# rate histogram
+rates = np.mean(spike_count_mat_E, axis=1).squeeze() / delta_t  # mean over trials
+plt.figure(figsize=(10, 5))
+plt.hist(rates, bins=40, range=[0, 15], alpha=.7)
+plt.title('Firing rates in spikes / sec')
+plt.axvline(np.mean(rates), linestyle='--', label='mean={}'.format(np.round(np.mean(rates), 2)), color='C1')
+plt.legend()
+plt.savefig('rate_hist_' + filename + 'df')
+
+# correlation histogram
+rho = correlations_E[0]
+plt.figure(figsize=(10, 5))
+plt.hist(rho, bins=40, range=[-.5, .5], alpha=.7)
+plt.axvline(np.mean(rho), linestyle='--', label='mean={}'.format(np.round(np.mean(rho), 2)), color='C1')
+plt.legend()
+plt.savefig('rho_hist_' + filename + 'df')
+plt.show()
--- lfmods/balanced_network_utils.py
+++ lfmods/balanced_network_utils.py
@@ -1,3 +1,9 @@
+import numpy as np
+import os
+import pickle
+import matplotlib.pyplot as plt
+
+
 def get_cluster_connection_probs(REE, k, p_ee):
     """
     When using clustered connectivity in the E population the avergae sparseness should still be p_ee.
@@ -11,3 +17,97 @@ def get_cluster_connection_probs(REE, k, p_ee):
     p_out = p_ee * k / (REE + k - 1)
     p_in = REE * p_out
     return p_in, p_out
+
+
+def plot_spike_trains(spikemonitor, time_setoff=0.):
+    spiketimes = np.asarray(spikemonitor.spike_trains())
+    for idx in range(len(spiketimes)):
+        times = spiketimes[idx][spiketimes[idx] > time_setoff]
+        plt.plot(times, idx * np.ones_like(times), '.b', markersize=1)
+    plt.xlabel('Time (ms)')
+    plt.ylabel('Neuron index')
+
+
+def plot_traces(statemonitor, time_setoff=0.):
+    time_mask = np.asarray(statemonitor.t) > time_setoff
+
+    plt.subplot(211)
+    plt.plot(statemonitor.t[time_mask], statemonitor.v[0, time_mask], label='vE')
+    plt.legend()
+    plt.subplot(212)
+    plt.plot(statemonitor.t[time_mask], statemonitor.I_e[0, time_mask], label='Ie')
+    plt.plot(statemonitor.t[time_mask], statemonitor.I_i[0, time_mask], label='Ii')
+    plt.legend(loc='best')
+    plt.xlabel('Time (ms)')
+
+
+def select_spiketimes(spiketimes, t, delta_t):
+    spiketimes = np.asarray(spiketimes)
+    timemask = np.logical_and(spiketimes >= t, spiketimes <= t + delta_t)
+    return spiketimes[timemask]
+
+
+def get_spiketimes_for_time_window(spiketime_dict, t, delta_t):
+    return {k: select_spiketimes(v, t, delta_t) for k, v in spiketime_dict.items()}
+
+
+def get_spikecounts_for_time_window(spiketime_dict, t, delta_t):
+    # get spike times for
+    spiketime_dict_window = get_spiketimes_for_time_window(spiketime_dict, t, delta_t)
+    spikecounts = [spiketime_array.size for spiketime_array in spiketime_dict_window.values()]
+    return np.array(spikecounts)
+
+
+def calculate_spike_counts_over_windows(spiketime_dict, t, delta_t, window_length):
+    n_neurons = len(spiketime_dict.keys())
+    length_of_recording = delta_t
+    n_time_windows = int(length_of_recording / window_length)
+    spike_counts_windows = np.zeros((n_neurons, n_time_windows))
+
+    for window_idx in range(n_time_windows):
+        wt = t + window_idx * window_length
+        spike_counts_windows[:, window_idx] = get_spikecounts_for_time_window(spiketime_dict,
+                                                                              t=wt,
+                                                                              delta_t=window_length)
+    return spike_counts_windows
+
+
+def calculate_correlation_matrix(spikecount_matrix_windows):
+    n_trials, n_neurons, n_time_windows = spikecount_matrix_windows.shape
+
+    # prelocate the cov matrix
+    cov = np.zeros((n_neurons, n_neurons))
+    for trial in range(n_trials):
+        # just add them up over trials
+        cov += np.cov(spikecount_matrix_windows[trial, ...])
+    # average across trials
+    cov /= n_trials
+
+    # get the mask of spiking neurons idx
+    spiking_mask = np.logical_not(np.diag(cov).copy() == 0)
+
+    # remove silent neurons from the analysis
+    temp_cov = cov[spiking_mask, :]
+    new_cov = temp_cov[:, spiking_mask]
+    var = np.diag(new_cov).copy()
+
+    # use the outer product over the variance vector to do it vectorized
+    rho = new_cov / np.sqrt(np.outer(var, var))
+    assert(np.sum(np.isnan(rho)) == 0), 'there are nan in the correlation matrix'
+
+    # remove the diagonal elements and the lower triangle because we are interested cross correlations only
+    matrix_mask = np.tril_indices(rho.shape[0])
+    rho[matrix_mask] = np.inf
+    reduced_rho = rho[np.isfinite(rho)]
+    return reduced_rho
+
+
+def save_data(data, filename, folder):
+    full_path_to_file = os.path.join(folder, filename + '.p')
+    with open(full_path_to_file, 'wb') as outfile:
+        pickle.dump(data, outfile, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def load_data(filename, folder):
+    full_path = os.path.join(folder, filename)
+    return pickle.load(open(full_path, 'rb'))
