ee00e65b32da203eea8c9d0d71d3e41d302f0ea8
jan-matthis <mail@jan-matthis.de>
2017-08-08 16:50:38 +0200

    empty repo (old version on feature branch)

--- Makefile
+++
@@ -1,6 +0,0 @@
-install:
-	python setup.py develop
-	python setup.py build_ext --inplace
-
-code-analysis:
-	flake8 ops | grep -v __init__
--- experiments/nips/fig_4.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-cd ..
-cd ..
-
-# autapse model
-python run.py autapse no_lr_s1 --samples 2000 --sim-kwargs pilot_samples=100 --seed 3 --iw-loss --debug --rep 10
-python run.py autapse lr_s1 --bad-data --samples 2000 --sim-kwargs pilot_samples=100 --seed 3 --iw-loss --debug --rep 10
