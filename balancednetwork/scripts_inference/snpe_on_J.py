6264866c2d41add9140057baee905f39094cb945
janfb <j.f.boelts@gmail.com>
2018-01-10 11:35:37 +0100

    update 4D weight inference script

--- balancednetwork/scripts_inference/snpe_on_J.py
+++ balancednetwork/scripts_inference/snpe_on_J.py
@@ -2,62 +2,86 @@ import delfi.distribution as dd
 import delfi.inference as infer
 import os
 import pickle
-import scipy.stats as st
 import time
 
-from lfimodels.balancednetwork.BalancedNetworkSimulator import BalancedNetwork
-from lfimodels.balancednetwork.BalancedNetworkStats import BalancedNetworkStats
-from lfimodels.balancednetwork.BalancedNetworkGenerator import BalancedNetworkGenerator
+try:
+    from lfimodels.balancednetwork.BalancedNetworkSimulator import BalancedNetwork
+    from lfimodels.balancednetwork.BalancedNetworkStats import BalancedNetworkStats, Identity
+    from lfimodels.balancednetwork.BalancedNetworkGenerator import BalancedNetworkGenerator
+    from utils import save_results
+except:
+    import sys
+    sys.path.append('../../../lfi-models')
+    sys.path.append('../')
+    from utils import save_results
+    from lfimodels.balancednetwork.BalancedNetworkSimulator import BalancedNetwork
+    from lfimodels.balancednetwork.BalancedNetworkStats import BalancedNetworkStats, Identity
+    from lfimodels.balancednetwork.BalancedNetworkGenerator import BalancedNetworkGenerator
 
+seed = 3
 n_params = 4
-n_cores_to_use = 4
+ree = 1.0
+n_cores_to_use = 32
 
-ntrain = 500
-n_minibatch = 100
-n_pilot_samples = 30
+ntrain = 32
+n_minibatch = 8
+n_pilot_samples = 0
 
 nrounds = 2
-save_data = True
-path_to_save_folder = '../data/'  # has to exist on your local path
+round_cl = 2
 
-m = BalancedNetwork(inference_params=['wxy'], dim=n_params, first_port=9000,
-                    verbose=True, n_servers=n_cores_to_use, duration=3.)
-p = dd.Uniform(lower=[0.01] * n_params, upper=[0.1] * n_params)
-s = BalancedNetworkStats(n_workers=n_cores_to_use)
+save_data = True
+
+# if True, calculates the summary stats on the fly to save memory
+stats_onthefly = True
+
+path_to_save_folder = '../results/'  # has to exist on your local path
+
+true_params = [0.024, 0.045, 0.014, 0.057]  # params from the paper
+
+s = Identity() if stats_onthefly else BalancedNetworkStats(n_workers=n_cores_to_use)
+
+m = BalancedNetwork(inference_params=['wxy'], dim=n_params, first_port=8100,
+                    verbose=True, n_servers=n_cores_to_use, duration=3., parallel=True,
+                    estimate_time=True, calculate_stats=stats_onthefly, seed=seed)
+
+p = dd.Uniform(lower=[0.8 * true_param for true_param in true_params],
+               upper=[1.3 * true_param for true_param in true_params], seed=seed)
+
 g = BalancedNetworkGenerator(model=m, prior=p, summary=s)
 
-# here we set the true params
-true_params = [[0.024, 0.045, 0.014, 0.057]]  # params from the paper
 # run forward model
 data = m.gen(true_params)
 # get summary stats
 stats_obs = s.calc(data[0])
 
 # set up inference
-res = infer.SNPE(g, obs=stats_obs, n_components=1, pilot_samples=n_pilot_samples)
+res = infer.SNPE(g, obs=stats_obs, n_components=3, pilot_samples=n_pilot_samples,
+                 prior_norm=True, seed=seed)
 
 # run with N samples
-out, trn_data, posteriors = res.run(ntrain, nrounds, epochs=1000, minibatch=n_minibatch)
+out, trn_data, posteriors = res.run(ntrain, nrounds, epochs=1000, minibatch=n_minibatch,
+                                    round_cl=round_cl)
 
 # evaluate the posterior at the observed data
 posterior = res.predict(stats_obs)
 
-result_dict = dict(true_params=true_params, stats_obs=stats_obs, nrouns=nrounds, ntrain=ntrain,
-                   posterior=posterior, out=out, trn_data=trn_data, posterior_list=posteriors)
+result_dict = dict(true_params=true_params, stats_obs=stats_obs, nrouns=nrounds, ntrain=ntrain, seed=seed,
+                   posterior=posterior, out=out, trn_data=trn_data, prior=p, posterior_list=posteriors)
+
+simulation_name = '{}_snpe_Jxy_r{}_n{}_rcl{}'.format(time.strftime('%Y%m%d%H%M'), nrounds,
+                                                     ntrain, round_cl).replace('.', '')
 
 # set up a dict for saving the results
-if save_data and os.path.exists(path_to_save_folder):
+# save the results
+if save_data:
+    path_to_file = save_results(result_dict, simulation_name, path_to_save_folder)
+    print(path_to_file)
 
-    filename = os.path.join(path_to_save_folder,
-                            '{}_snpe_J_r{}_ntrain{}'.format(time.time(), nrounds, ntrain).replace('.', '') + '.p')
+# extract the posterior
+n_components = len(posterior.a)
+means = [posterior.xs[c].m for c in range(n_components)]
+Ss = [posterior.xs[c].S for c in range(n_components)]
 
-    with open(filename, 'wb') as handle:
-        pickle.dump(result_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    print(filename)
-elif save_data:
-    print('Path does not exist: {} saving in .'.format(path_to_save_folder))
-
-    filename = '{}_snpe_J_r{}_ntrain{}'.format(time.time(), nrounds, ntrain).replace('.', '') + '.p'
-
-    with open(filename, 'wb') as handle:
-        pickle.dump(result_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+print('Predicited: {} +- {}'.format(means, Ss))
+print('True: {}'.format(true_params))
