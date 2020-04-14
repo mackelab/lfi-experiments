3a2f849b12d1972e2059cfa503f1c2d61a7f1dca
pedro <ppjgoncalves@gmail.com>
2020-04-14 10:37:06 +0200

    updated multiprocessing LRZ code

--- bursting_biophysics/Arco_cell_samples.py
+++ bursting_biophysics/Arco_cell_samples.py
@@ -1,26 +1,19 @@
-import model.utils as utils
 import multiprocessing
 import multiprocessing.pool
 import numpy as np
 import os
 import sys
 import time
+import utils
 
 from __future__ import division
+from parameter_setup import load_ground_truth_params
 
 ################################################################################
 # seeding by time stamp
 seed1 = time.time()
 seed = int((seed1 % 1)*1e7)
 rng = np.random.RandomState(seed=seed)
-
-################################################################################
-# directory for simulation
-dir_path = '$HOME/in_silico_framework/'
-sys.path.append(dir_path)
-import Interface as I
-
-from biophysics_fitting import hay_complete_default_setup, L5tt_parameter_setup
 
 ################################################################################
 class NoDaemonProcess(multiprocessing.Process):
@@ -43,28 +36,28 @@ class MyPool(multiprocessing.pool.Pool):
         kwargs['context'] = NoDaemonContext()
         super(MyPool, self).__init__(*args, **kwargs)
 
-################################################################################
-true_params = utils.obs_params()
 
-# define prior
-seed_p = rng.randint(1e7)
-p = utils.prior(true_params=true_params,seed=seed_p)
+################################################################################
+# ground-truth parameters
+gt = load_ground_truth_params()
+
 
 ################################################################################
 # sample from prior and run simulations
-n_sims = 1000
+n_sims = 20000
 
 # sample from prior
-params = p.gen(n_sims)
+seed_p = rng.randint(1e7)
+params = utils.prior_around_gt(gt=gt, fraction_of_full_prior=0.1, num_samples=n_sims, seed=seed_p)
 
 # number of parallel processes (smaller or equal to n_sims)
-n_processes = 10
+n_processes = 28
 
 # simulate
 seeds_model = rng.randint(1e7,size=n_sims)
 def sim_f(param):
     x = utils.simulator_wrapper(param[1:],seed=int(param[0]))
-    return utils.summary_stats(x)
+    return utils.summary_stats(x, n_xcorr=0, n_mom=4)
 
 data = []
 params_seed = np.concatenate((seeds_model.reshape(-1,1),params),axis=1)
